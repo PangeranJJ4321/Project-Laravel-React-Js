e6d8c95d6d7b16c90fd0ab91b180d949c18ff819
unknown <pangeranjuhrifar@gmail.com>
2025-02-11 10:31:37 +0700

    melakukan axios fetching api categories dan membuat props interface dengan typescript

---
+++ BookRecipe/frontend-book-recipe/src/types/Type.tsx
@@ -0,0 +1,46 @@
+
+interface Author {
+    id: string,
+    name: string,
+    photo: string,
+} 
+
+interface Tutorial {
+    id: number,
+    name: string,
+}
+interface Ingredient {
+    id: number,
+    name: string,
+    photo: string,
+}
+
+interface RecipeIngredient {
+    id: number,
+    ingredient: Ingredient,
+}
+
+export interface Category {
+    id: number,
+    name: string,
+    slug: string,
+    icon: string,
+    recipe_count: number,
+    recipes?: Recipe[],
+}
+
+export interface Recipe {
+    id: string,
+    name: string,
+    slug: string,
+    url_file: string,
+    url_video: string,
+    author: Author,
+    category: Category,
+    thumbnail: string,
+    tutorials: Tutorial[],
+    recipe_ingredients: RecipeIngredient[],
+    about: string,
+}
+
+
