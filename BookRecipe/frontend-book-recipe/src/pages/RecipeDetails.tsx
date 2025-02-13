331267f0e94a8d7044c111eb08ec8ccf2ceaf55e
unknown <pangeranjuhrifar@gmail.com>
2025-02-13 23:03:28 +0700

    fetching api data recipe berdasarkan slug

--- BookRecipe/frontend-book-recipe/src/pages/RecipeDetails.tsx
+++ BookRecipe/frontend-book-recipe/src/pages/RecipeDetails.tsx
@@ -1,10 +1,32 @@
+import { useParams } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import About from "../components/RecipeDetails/About";
 import DownloadRecipe from "../components/RecipeDetails/DownloadRecipe";
 import Header from "../components/RecipeDetails/Header";
 import ListDetails from "../components/RecipeDetails/ListDetails";
+import { useEffect, useState } from "react";
+import { Recipe } from "../types/Type";
+import axios from "axios";
 
 const RecipeDetails = () => {
+
+    const {slug} = useParams< {slug: string} >();
+    const [recipe, setRecipe] = useState<Recipe | null>(null);
+    const [loading, setLoading] = useState(true);
+    const [error, seterror] = useState<string | null>(null);
+
+    useEffect( () => {
+        axios.get(`http://127.0.0.1:8000/api/recipe/${slug}`)
+        .then((response) => {
+            setRecipe(response.data.data);
+            setLoading(false);
+        })
+        .catch ((error) => {
+            seterror(error.message);
+            setLoading(!loading);
+        });
+    }, [slug]);
+
     return (
         <>
             <Navbar title="" showBackButton={true} showMoreButton={true} />
