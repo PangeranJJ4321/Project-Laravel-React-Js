e7aa60148d5e135ca545cc0079381cd3fd85930e
unknown <pangeranjuhrifar@gmail.com>
2025-02-13 22:50:02 +0700

    Mengoptimalkan component ListDetails

---
+++ BookRecipe/frontend-book-recipe/src/components/RecipeDetails/ReviewRecipe.tsx
@@ -0,0 +1,71 @@
+const ReviewRecipe = () => {
+    return (
+        <div className="flex flex-col gap-5">
+            <div className="review-card flex flex-col pb-5 border-b border-[#E7E7E7] gap-3">
+                <p className="leading-8">Hemat uang jadinya gaperlu beli burger di luar, udah bisa masak sendiri hehehe</p>
+                <div className="flex items-center justify-between gap-3">
+                    <div className="flex items-center gap-3">
+                        <div className="flex shrink-0 w-[50px] h-[50px] rounded-full overflow-hidden">
+                            <img src="/assets/images/photos/photo-2.png" className="w-full h-full object-cover" alt="avatar" />
+                        </div>
+                        <div className="flex flex-col gap-[2px]">
+                            <p className="font-semibold">Shayna</p>
+                            <p className="text-sm leading-[21px] text-[#848486]">12 Days Ago</p>
+                        </div>
+                    </div>
+                    <div className="flex items-center">
+                        <img src="/assets/images/icons/Star 1.svg" className="w-[18px] h-[18px]" alt="star" />
+                        <img src="/assets/images/icons/Star 1.svg" className="w-[18px] h-[18px]" alt="star" />
+                        <img src="/assets/images/icons/Star 1.svg" className="w-[18px] h-[18px]" alt="star" />
+                        <img src="/assets/images/icons/Star 1.svg" className="w-[18px] h-[18px]" alt="star" />
+                        <img src="/assets/images/icons/Star 1.svg" className="w-[18px] h-[18px]" alt="star" />
+                    </div>
+                </div>
+            </div>
+            <div className="review-card flex flex-col pb-5 border-b border-[#E7E7E7] gap-3">
+                <p className="leading-8">Recipe paling enak menurut gue karena anak gak pernah bosan makan ini terus setiap harinya, thank you banget ya shay...</p>
+                <div className="flex items-center justify-between gap-3">
+                    <div className="flex items-center gap-3">
+                        <div className="flex shrink-0 w-[50px] h-[50px] rounded-full overflow-hidden">
+                            <img src="/assets/images/photos/photo-3.png" className="w-full h-full object-cover" alt="avatar" />
+                        </div>
+                        <div className="flex flex-col gap-[2px]">
+                            <p className="font-semibold">ShSarina Dwinaayna</p>
+                            <p className="text-sm leading-[21px] text-[#848486]">12 Days Ago</p>
+                        </div>
+                    </div>
+                    <div className="flex items-center">
+                        <img src="/assets/images/icons/Star 1.svg" className="w-[18px] h-[18px]" alt="star"/>
+                        <img src="/assets/images/icons/Star 1.svg" className="w-[18px] h-[18px]" alt="star"/>
+                        <img src="/assets/images/icons/Star 1.svg" className="w-[18px] h-[18px]" alt="star"/>
+                        <img src="/assets/images/icons/Star 1.svg" className="w-[18px] h-[18px]" alt="star"/>
+                        <img src="/assets/images/icons/Star-grey.svg" className="w-[18px] h-[18px]" alt="star"/>
+                    </div>
+                </div>
+            </div>
+            <div className="review-card flex flex-col pb-5 border-b border-[#E7E7E7] gap-3">
+                <p className="leading-8">Hemat uang jadinya gaperlu beli burger di luar, udah bisa masak sendiri hehehe</p>
+                <div className="flex items-center justify-between gap-3">
+                    <div className="flex items-center gap-3">
+                        <div className="flex shrink-0 w-[50px] h-[50px] rounded-full overflow-hidden">
+                            <img src="/assets/images/photos/photo-4.png" className="w-full h-full object-cover" alt="avatar" />
+                        </div>
+                        <div className="flex flex-col gap-[2px]">
+                            <p className="font-semibold">Alqowy Putri</p>
+                            <p className="text-sm leading-[21px] text-[#848486]">12 Days Ago</p>
+                        </div>
+                    </div>
+                    <div className="flex items-center">
+                        <img src="/assets/images/icons/Star 1.svg" className="w-[18px] h-[18px]" alt="star" />
+                        <img src="/assets/images/icons/Star 1.svg" className="w-[18px] h-[18px]" alt="star" />
+                        <img src="/assets/images/icons/Star 1.svg" className="w-[18px] h-[18px]" alt="star" />
+                        <img src="/assets/images/icons/Star 1.svg" className="w-[18px] h-[18px]" alt="star" />
+                        <img src="/assets/images/icons/Star 1.svg" className="w-[18px] h-[18px]" alt="star" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ReviewRecipe;
---
+++ BookRecipe/frontend-book-recipe/src/components/RecipeDetails/TutorialRecipe.tsx
@@ -0,0 +1,40 @@
+const TutorialRecipe = () => {
+    return (
+        <>
+            <iframe className="w-full aspect-video rounded-[20px] bg-[#D9D9D9]" src="https://www.youtube.com/embed/n1YeqIlbkxc" />
+            <div className="list-items-container flex flex-col mt-[26px]">
+                <div className="list flex gap-[14px]">
+                    <div className="flex relative">
+                        <div className="relative z-10 flex shrink-0 items-center justify-center w-10 h-10 rounded-full bg-white shadow-[0_12px_30px_0_#D6D6D680] font-bold text-sm leading-[21px]">
+                            <span>1</span>
+                        </div>
+                        <div className="line absolute left-1/2 transform -translate-x-1/2 h-full border-dashed border border-[#DEDFEB]"></div>
+                    </div>
+                    <p className="leading-8 pb-[30px]">Siapkan telur rebus dicampur dengan mentega rendah kalori</p>
+                </div>
+
+                <div className="list flex gap-[14px]">
+                    <div className="flex relative">
+                        <div className="relative z-10 flex shrink-0 items-center justify-center w-10 h-10 rounded-full bg-white shadow-[0_12px_30px_0_#D6D6D680] font-bold text-sm leading-[21px]">
+                            <span>2</span>
+                        </div>
+                        <div className="line absolute left-1/2 transform -translate-x-1/2 h-full border-dashed border border-[#DEDFEB]"></div>
+                    </div>
+                    <p className="leading-8 pb-[30px]">Panaskan minyak dengan api stabil agar tidak gosong nanti dagingnya</p>
+                </div>
+                            
+                <div className="list flex gap-[14px]">
+                    <div className="flex relative">
+                        <div className="relative z-10 flex shrink-0 items-center justify-center w-10 h-10 rounded-full bg-white shadow-[0_12px_30px_0_#D6D6D680] font-bold text-sm leading-[21px]">
+                            <span>3</span>
+                        </div>
+                        <div className="line absolute left-1/2 transform -translate-x-1/2 h-full border-dashed border border-[#DEDFEB]"></div>
+                    </div>
+                    <p className="leading-8 pb-[30px]">Roti disajikan tanpa wijen agar rasanya tidak bertabrakan salada</p>
+                </div>
+            </div>                     
+        </>
+    );
+}
+
+export default TutorialRecipe;
