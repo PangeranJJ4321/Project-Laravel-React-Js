fdaba60f560f80ae859e1e1da610c8b0b348a2fa
unknown <pangeranjuhrifar@gmail.com>
2025-02-18 22:39:59 +0700

    membuai model dan migration apikey

--- BookRecipe/Laravel-BookRecipe/database/migrations/2025_02_18_152918_create_api_keys_table.php
+++ BookRecipe/Laravel-BookRecipe/database/migrations/2025_02_18_152918_create_api_keys_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('api_keys', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('key')->unique();
             $table->timestamps();
         });
     }
