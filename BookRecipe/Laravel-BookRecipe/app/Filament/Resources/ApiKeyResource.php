874d14a9b44e43a0c52bd0cd16c71499a68a276b
unknown <pangeranjuhrifar@gmail.com>
2025-02-18 22:52:18 +0700

    membuat filament resource untuk apikey, diaman disitu aku buat formnya gitulah, btw ini konsepnya sama sepeti kita menggunakan api key disebuah platform yang menyediakan api key gitu, tapi disini aku buatnya manual✌️

--- BookRecipe/Laravel-BookRecipe/app/Filament/Resources/ApiKeyResource.php
+++ BookRecipe/Laravel-BookRecipe/app/Filament/Resources/ApiKeyResource.php
@@ -23,7 +23,13 @@ class ApiKeyResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Forms\Components\TextInput::make('name')
+                ->required()
+                ->maxLength(225),
+
+                Forms\Components\TextInput::make('key')
+                ->required()
+                ->maxLength(225),
             ]);
     }
 
@@ -31,13 +37,15 @@ class ApiKeyResource extends Resource
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('name')
+                ->searchable(),
             ])
             ->filters([
                 //
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
