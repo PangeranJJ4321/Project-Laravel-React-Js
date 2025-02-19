490382e405dee91a9b933c52fc7b2bfd51f11f80
unknown <pangeranjuhrifar@gmail.com>
2025-02-19 13:14:46 +0700

    Mengoptimalkan component BottomNav, dengan membuat perulangan pada componentnya agar lebih flexsibel

--- BookRecipe/frontend-book-recipe/src/components/home/BottomNav.tsx
+++ BookRecipe/frontend-book-recipe/src/components/home/BottomNav.tsx
@@ -1,3 +1,21 @@
+interface BottomNavItemProps {
+    href: string;
+    icon: string;
+    label: string;
+    active: boolean;
+}
+
+const BottomNavItem: React.FC<BottomNavItemProps> = ({ href, icon, label, active }) => {
+    return (
+        <a href={href} className={`nav-items ${active ? "font-semibold" : "text-gray-500"}`}>
+            <div className="flex flex-col items-center text-center gap-[7px] text-sm leading-[21px]">
+                <img src={`/assets/images/icons/${icon}`} className="w-6 h-6" alt={`${label} icon`} />
+                <span>{label}</span>
+            </div>
+        </a>
+    );
+};
+
 const BottomNav = () => {
     const navItems = [
         { href: "/", icon: "note-favorite-orange.svg", label: "Browse", active: true },
@@ -9,13 +27,8 @@ const BottomNav = () => {
     return (
         <nav id="BottomNav" className="fixed z-50 bottom-0 w-full max-w-[640px] mx-auto border-t border-[#E7E7E7] py-4 px-5 bg-white/70 backdrop-blur">
             <div className="flex items-center justify-evenly">
-                {navItems.map(({ href, icon, label, active }, index) => (
-                    <a key={index} href={href} className={`nav-items ${active ? "font-semibold" : "text-gray-500"}`}>
-                        <div className="flex flex-col items-center text-center gap-[7px] text-sm leading-[21px]">
-                            <img src={`/assets/images/icons/${icon}`} className="w-6 h-6" alt={`${label} icon`} />
-                            <span>{label}</span>
-                        </div>
-                    </a>
+                {navItems.map((item, index) => (
+                    <BottomNavItem key={index} {...item} />
                 ))}
             </div>
         </nav>
