21ec3cbd4dc787e87742d03ff8117e9c6efe8a4b
unknown <pangeranjuhrifar@gmail.com>
2025-02-27 08:27:52 +0700

    Menmabahkan tampilan untuk halaman Office Details

--- RentOffice/frontend-ren-office/src/App.tsx
+++ RentOffice/frontend-ren-office/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Browse from './pages/Browse'
 import CityDetails from './pages/CityDetails'
+import OfficeDetails from './pages/OfficeDetails'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Browse />} />
           <Route path='/city/:slug' element={<CityDetails />} />
+          <Route path='/office/:slug' element={<OfficeDetails />} />
         </Routes>
       </BrowserRouter> 
     </>
